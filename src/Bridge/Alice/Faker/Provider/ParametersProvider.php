0aa7514e9284f925b5276c74a9ad08bad2079d42
tg666 <tomasglawaty@icloud.com>
2020-08-03 22:32:45 +0200

    - added the second argument `$nested` into method `ParametersProvider::parameter()`, the method supports a dot notation - updated Alice's docs

--- src/Bridge/Alice/Faker/Provider/ParametersProvider.php
+++ src/Bridge/Alice/Faker/Provider/ParametersProvider.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace SixtyEightPublishers\FixturesBundle\Bridge\Alice\Faker\Provider;
 
 use Nelmio\Alice\ParameterBag;
+use Nelmio\Alice\Throwable\Exception\ParameterNotFoundException;
+use Nelmio\Alice\Throwable\Exception\ParameterNotFoundExceptionFactory;
 
 /**
  * Workaround for an issue https://github.com/nelmio/alice/issues/902
@@ -21,12 +23,24 @@ final class ParametersProvider
 
 	/**
 	 * @param string $name
+	 * @param bool   $nested
 	 *
 	 * @return mixed
 	 */
-	public function parameter(string $name)
+	public function parameter(string $name, bool $nested = TRUE)
 	{
-		return $this->parameterBag->get($name);
+		if (TRUE === $nested && FALSE !== strpos($name, '.')) {
+			$parts = explode('.', $name);
+			$name = array_shift($parts);
+		}
+
+		$parameter = $this->parameterBag->get($name);
+
+		if (isset($parts) && !empty($parts)) {
+			$parameter = $this->filterParameterParts($parameter, $name, $parts);
+		}
+
+		return $parameter;
 	}
 
 	/**
@@ -38,5 +52,35 @@ final class ParametersProvider
 	public function setParameterBag(ParameterBag $parameterBag): void
 	{
 		$this->parameterBag = $parameterBag;
+	}
+
+	/**
+	 * @param mixed  $parameter
+	 * @param string $rootName
+	 * @param array  $parts
+	 *
+	 * @return mixed
+	 */
+	private function filterParameterParts($parameter, string $rootName, array $parts)
+	{
+		foreach ($parts as $part) {
+			if (!is_array($parameter)) {
+				throw new ParameterNotFoundException(sprintf(
+					'Can not resolve a parameter %s.%s because the parameter %s is not an array.',
+					$rootName,
+					$part,
+					$rootName
+				));
+			}
+
+			if (!array_key_exists($part, $parameter)) {
+				throw ParameterNotFoundExceptionFactory::create($rootName . '.' . $part);
+			}
+
+			$parameter = $parameter[$part];
+			$rootName .= '.' . $part;
+		}
+
+		return $parameter;
 	}
 }
