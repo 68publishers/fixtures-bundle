cbf2afb482991f9f6888e43521d62ad4f3e864f4
tg666 <tomasglawaty@icloud.com>
2020-08-11 17:01:47 +0200

    Basic ObjectLoaders
    - added ObjectLoaders `Find`, `FindAll`, `FindBy` and `FindOneBy` for the `ObjectManager`
    - added more generic ObjectLoaders `Find`, `FindAll`, `FindBy` and `FindOneBy` that internally calls a specific ObjectLoaders (e.g. loaders for the ObjectManager)

---
+++ src/Loader/ObjectLoader/ObjectManager/FindAll.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SixtyEightPublishers\FixturesBundle\Loader\ObjectLoader\ObjectManager;
+
+use Doctrine\Common\Persistence\ObjectManager;
+
+final class FindAll extends ObjectManagerLoader
+{
+	/**
+	 * {@inheritDoc}
+	 */
+	protected function doLoad(ObjectManager $manager): array
+	{
+		return $this->combineObjectsWithIds(
+			$manager->getRepository($this->getClassName())->findAll()
+		);
+	}
+}
